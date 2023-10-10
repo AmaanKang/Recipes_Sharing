6b916dc302309184d3a115977f6b07b589bc17ca
Amandeep Kaur <amankang344@gmail.com>
2023-10-09 22:23:18 -0400

    Working on adding a user to Users table by using the Controller API.

--- Recipes/src/main/java/com/amandeep/Recipes/UserController.java
+++ Recipes/src/main/java/com/amandeep/Recipes/UserController.java
@@ -3,15 +3,11 @@ package com.amandeep.Recipes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/users")
 
