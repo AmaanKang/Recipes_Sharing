2d5456b16564eebc2c53aa0372b005fbbfd835ef
Amandeep Kaur <amankang344@gmail.com>
2023-10-15 14:56:42 -0400

    Login calls

--- Recipes/src/main/java/com/amandeep/Recipes/UserController.java
+++ Recipes/src/main/java/com/amandeep/Recipes/UserController.java
@@ -14,6 +14,16 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @PostMapping
+    public int loginUser(@RequestBody Map<String,String> payload){
+        if(userService.allUsers().contains(new User(payload.get("emailAddress"),payload.get("password")))){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody Map<String,String> payload){
         return new ResponseEntity<>(userService.createUser(payload.get("emailAddress"),payload.get("password")), HttpStatus.CREATED);
--- Recipes/src/main/java/com/amandeep/Recipes/UserService.java
+++ Recipes/src/main/java/com/amandeep/Recipes/UserService.java
@@ -3,14 +3,20 @@ package com.amandeep.Recipes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    public List<User> allUsers(){
+        return userRepository.findAll();
+    }
+
+
     public User createUser(String emailAddress, String password){
         User user = new User(emailAddress,password);
-
         userRepository.insert(user);
         return user;
     }
